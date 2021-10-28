9ccc177ed6f821e6b0986f8acee78ada7a9af7c9
Tik88 <xtikx@yandex.ru>
2021-10-28 16:30:36 +0300

    Доработка

---
+++ Lab7.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Вводим данные 
+            Console.WriteLine("Вводим длины ребер куба");
+            int a = Convert.ToInt32(Console.ReadLine());           
+            #endregion
+            #region Условия и вывод
+            double V, S;
+            CubeVolume(a, out V);
+            CubeArea(a, out S);
+            Console.WriteLine("Объём куба {0:F2}", V);
+            Console.WriteLine("Площадь куба {0:F2}", S);
+            Console.ReadKey();
+            #endregion
+        }
+        #region Методы
+        static void CubeVolume(int a, out double V)
+        {
+            V = Convert.ToInt32(Math.Pow(a, 3));
+        }
+        static void CubeArea(int a, out double S)
+        {
+            S = 6 * ((Math.Pow(a, 2)));
+        }
+        #endregion
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace Lab7
             int y2 = Convert.ToInt32(Console.ReadLine());
             int z2 = Convert.ToInt32(Console.ReadLine());
             #endregion
+            #region Условия и вывод
             double p1, p2, S1, S2;
             Triangle1(x1, y1, z1, out p1, out S1);
             Triangle2(x2, y2, z2, out p2, out S2);
@@ -34,7 +35,9 @@ namespace Lab7
                 Console.WriteLine("Площадь второго треугольника больше площади первого");
             }
             Console.ReadKey();
+            #endregion
         }
+        #region Методы
         static void Triangle1(int x1, int y1, int z1, out double p1, out double S1)
         {
             p1 = (x1 + y1 + z1) / 2;
@@ -45,6 +48,7 @@ namespace Lab7
             p2 = (x2 + y2 + z2) / 2;
             S2 = Math.Sqrt(Math.Abs(p2 * ((p2 - x2) * (p2 - y2) * (p2 - z2))));
         }
-        
+        #endregion
+
     }
 }
